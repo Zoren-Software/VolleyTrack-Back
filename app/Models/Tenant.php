2a7dc9213449ee93311cd0514bc7b1f027f74655
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-03-12 19:11:34 -0300

    feat: Primeiro commit, start projeto + Laravel Tenancy

---
+++ app/Models/Tenant.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
+use Stancl\Tenancy\Contracts\TenantWithDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDomains;
+
+class Tenant extends BaseTenant implements TenantWithDatabase
+{
+    use HasDatabase, HasDomains;
+}
