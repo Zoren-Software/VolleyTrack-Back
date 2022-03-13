a38a0c8b57a1c450709801c84182d776d5853a76
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-03-13 15:44:33 -0300

    feat: ajustado esquema de testes para tenants

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,36 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Stancl\Tenancy\Database\Models\Tenant;
+//use App\Models\Tenant;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected $tenancy = false;
+    public $tenantUrl;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        if ($this->tenancy) {
+            $this->initializeTenancy();
+            $this->tenantUrl = 'http://' . env('TENANT_TEST', 'test') . '.' . env('APP_HOST');
+        }
+    }
+
+    public function initializeTenancy(): void
+    {
+        $domain = env('TENANT_TEST', 'test');
+
+        if(!Tenant::find($domain)) {
+            $tenant = Tenant::create(['id' =>  env('TENANT_TEST', 'test')]);
+            $tenant->domains()->create(['domain' =>  env('TENANT_TEST', 'test')]);
+            $domain = $tenant;
+        }
+
+        tenancy()->initialize($domain);
+    }
 }
