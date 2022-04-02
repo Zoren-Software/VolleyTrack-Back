3fc289f289d220e3dc325e3922ce485e2d63f666
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-04-02 15:59:35 -0300

    style: ajustes php cs fixer

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -2,14 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
 use Stancl\Tenancy\Contracts\TenantWithDatabase;
 use Stancl\Tenancy\Database\Concerns\HasDatabase;
 use Stancl\Tenancy\Database\Concerns\HasDomains;
+use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
 
 class Tenant extends BaseTenant implements TenantWithDatabase
 {
-    use HasDatabase, HasDomains;
+    use HasDatabase;
+    use HasDomains;
 }
