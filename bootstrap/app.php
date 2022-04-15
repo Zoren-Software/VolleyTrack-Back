d7a0ebe529dbe121ec18d73d746a975062ea56e9
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-04-15 16:43:52 -0300

    feat: adicionado GraphQL Playground a aplicação

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,9 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+// NOTE - Adicionado GraphQL Playground
+//$app->register(MLL\GraphQLPlayground\GraphQLPlaygroundServiceProvider::class);
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
