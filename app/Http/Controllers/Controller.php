9ddc8a9effcebe9bd5b46bea6c3f37d590fcb68c
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-04-15 15:46:28 -0300

    style: ajustes php cs fixer

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,6 +10,8 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests;
+
     use DispatchesJobs;
+
     use ValidatesRequests;
 }
