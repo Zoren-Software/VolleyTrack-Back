d4f1cc299e37188041d8ba8204261d0c302723aa
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-05-22 19:45:18 -0300

    feat(team): feito cadastro de times #13

---
+++ app/GraphQL/Mutations/TeamMutation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\Team;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+final class TeamMutation
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function create($rootValue, array $args, GraphQLContext $context)
+    {
+        $team = new Team();
+        $team->name = $args['name'];
+        $team->user_id = $args['user_id'];
+        $team->save();
+
+        return $team;
+    }
+}
--- tests/Feature/GraphQL/UserTest.php
+++ tests/Feature/GraphQL/UserTest.php
@@ -4,13 +4,10 @@ namespace Tests\Feature\GraphQL;
 
 use App\Models\User;
 use Faker\Factory as Faker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
-    // use RefreshDatabase;
-
     protected $graphql = true;
 
     protected $tenancy = true;
@@ -25,48 +22,59 @@ class UserTest extends TestCase
     public function test_users_list()
     {
         User::factory()->make()->save();
-        $response = $this->graphQL(/** @lang GraphQL */ '
-            users {
-                paginatorInfo {
-                    count
-                    currentPage
-                    firstItem
-                    hasMorePages
-                    lastItem
-                    lastPage
-                    perPage
-                    total
-                }
-                data {
-                    id
-                    name
-                    email
-                    email_verified_at
-                    created_at
-                    updated_at
-                }
-            }
-        ')->assertJsonStructure([
+
+        $response = $this->graphQL(
+            'users',
+            [
+                'name' => '%%',
+                'first' => 10,
+                'page' => 1,
+            ],
+            [
+                'paginatorInfo' => [
+                    'count',
+                    'currentPage',
+                    'firstItem',
+                    'lastItem',
+                    'lastPage',
+                    'perPage',
+                    'total',
+                    'hasMorePages'
+                ],
+                'data' => [
+
+                    'id',
+                    'name',
+                    'email',
+                    'createdAt',
+                    'updatedAt',
+
+                ],
+            ],
+            'query',
+            false
+        );
+
+        $response->assertJsonStructure([
             'data' => [
                 'users' => [
                     'paginatorInfo' => [
                         'count',
                         'currentPage',
                         'firstItem',
-                        'hasMorePages',
                         'lastItem',
                         'lastPage',
                         'perPage',
-                        'total'
+                        'total',
+                        'hasMorePages',
                     ],
                     'data' => [
                         '*' => [
                             'id',
                             'name',
                             'email',
-                            'email_verified_at',
-                            'created_at',
-                            'updated_at'
+                            'createdAt',
+                            'updatedAt'
                         ]
                     ]
                 ],
@@ -83,61 +91,64 @@ class UserTest extends TestCase
      */
     public function test_user_info()
     {
-        //$this->withoutExceptionHandling();
         $user = User::factory()->make();
         $user->save();
 
-        $response = $this->graphQL(/** @lang GraphQL */ "
-            user (id: $user->id) {
-                id
-                name
-                email
-                email_verified_at
-                created_at
-                updated_at
-            }
-        ");
+        $saida = [
+            'id',
+            'name',
+            'email',
+            'emailVerifiedAt',
+            'createdAt',
+            'updatedAt'
+        ];
+
+        $response = $this->graphQL(
+            'user',
+            [
+                'id' => $user->id,
+            ],
+            $saida,
+            'query',
+            false
+        );
 
         $response->assertJsonStructure([
             'data' => [
-                'user' => [
-                    'id',
-                    'name',
-                    'email',
-                    'email_verified_at',
-                    'created_at',
-                    'updated_at'
-                ],
+                'user' => $saida,
             ],
         ])->assertStatus(200);
     }
 
     /**
-     * Listagem de um usuário
+     * Método de criação de um usuário.
      *
      * @dataProvider userCreateProvider
      * @author Maicon Cerutti
      *
      * @return void
      */
-    public function test_user_create($password, $email, $type_message_error, $expected_message, $expected)
+    public function test_user_create($parameters, $type_message_error, $expected_message, $expected)
     {
         $faker = Faker::create();
 
-        $response = $this->graphQL(/** @lang GraphQL */ '
-            userCreate (
-                name: "' . $faker->name . '"
-                email: "' . $email . '"
-                password: "' . $password . '"
-            ) {
-                id
-                name
-                email
-                email_verified_at
-                created_at
-                updated_at
-            }
-        ', 'mutation');
+        $parameters['name'] = $faker->name;
+
+        $response = $this->graphQL(
+            'userCreate',
+            $parameters,
+            [
+                'id',
+                'name',
+                'email',
+                'emailVerifiedAt',
+                'createdAt',
+                'updatedAt'
+            ],
+            'mutation',
+            false,
+            true
+        );
 
         if ($type_message_error) {
             $this->assertSame($response->json()['errors'][0]['extensions']['validation'][$type_message_error][0], trans($expected_message));
@@ -158,9 +169,33 @@ class UserTest extends TestCase
         $emailExistent = $faker->email;
 
         return [
+            'create user, success' => [
+                [
+                    'name' => $faker->name,
+                    'email' => $emailExistent,
+                    'password' => '123456',
+                ],
+                'type_message_error' => false,
+                'expected_message' => false,
+                'expected' => [
+                    'data' => [
+                        'userCreate' => [
+                            'id',
+                            'name',
+                            'email',
+                            'emailVerifiedAt',
+                            'createdAt',
+                            'updatedAt'
+                        ],
+                    ],
+                ],
+            ],
             'text password less than 6 characters, expected error' => [
-                'password' => '12345',
-                'email' => $faker->email,
+                [
+                    'name' => $faker->name,
+                    'email' => $faker->email,
+                    'password' => '12345',
+                ],
                 'type_message_error' => 'password',
                 'expected_message' => 'UserCreate.password_min_6',
                 'expected' => [
@@ -179,8 +214,10 @@ class UserTest extends TestCase
                 ],
             ],
             'no text password, expected error' => [
-                'password' => '',
-                'email' => $faker->email,
+                [
+                    'password' => ' ',
+                    'email' => $faker->email,
+                ],
                 'type_message_error' => 'password',
                 'expected_message' => 'UserCreate.password_required',
                 'expected' => [
@@ -198,27 +235,12 @@ class UserTest extends TestCase
                     ]
                 ],
             ],
-            'create user, success' => [
-                'password' => '123456',
-                'email' => $emailExistent,
-                'type_message_error' => false,
-                'expected_message' => false,
-                'expected' => [
-                    'data' => [
-                        'userCreate' => [
-                            'id',
-                            'name',
-                            'email',
-                            'email_verified_at',
-                            'created_at',
-                            'updated_at'
-                        ],
-                    ],
-                ],
-            ],
             'text password with 6 characters, success' => [
-                'password' => '123456',
-                'email' => $faker->email,
+                [
+                    'name' => $faker->name,
+                    'email' => $faker->email,
+                    'password' => '123456',
+                ],
                 'type_message_error' => false,
                 'expected_message' => false,
                 'expected' => [
@@ -227,16 +249,19 @@ class UserTest extends TestCase
                             'id',
                             'name',
                             'email',
-                            'email_verified_at',
-                            'created_at',
-                            'updated_at'
+                            'emailVerifiedAt',
+                            'createdAt',
+                            'updatedAt'
                         ],
                     ],
                 ],
             ],
             'email field is required, expected error' => [
-                'password' => '123456',
-                'email' => '',
+                [
+                    'name' => $faker->name,
+                    'password' => '123456',
+                    'email' => ' ',
+                ],
                 'type_message_error' => 'email',
                 'expected_message' => 'UserCreate.email_required',
                 'expected' => [
@@ -255,8 +280,11 @@ class UserTest extends TestCase
                 ],
             ],
             'email field is not unique, expected error' => [
-                'password' => '123456',
-                'email' => $emailExistent,
+                [
+                    'name' => $faker->name,
+                    'password' => '123456',
+                    'email' => $emailExistent,
+                ],
                 'type_message_error' => 'email',
                 'expected_message' => 'UserCreate.email_unique',
                 'expected' => [
@@ -275,8 +303,11 @@ class UserTest extends TestCase
                 ],
             ],
             'email field is not email valid, expected error' => [
-                'password' => '123456',
-                'email' => 'notemail.com',
+                [
+                    'name' => $faker->name,
+                    'password' => '123456',
+                    'email' => 'notemail.com',
+                ],
                 'type_message_error' => 'email',
                 'expected_message' => 'UserCreate.email_is_valid',
                 'expected' => [
