dedfc6c863faa8466fd883e5ca92787f76452857
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-05-23 00:03:27 -0300

    feat: adicionado função de edição de usuários

---
+++ lang/en/UserEdit.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'email_is_valid' => 'The email field must be a valid email address.',
+    'email_required' => 'The email field is required.',
+    'email_unique' => 'This email has already been registered.',
+    'password_min_6' => 'The password must be at least 6 characters long.',
+    'password_required' => 'The password field is required.',
+
+];
