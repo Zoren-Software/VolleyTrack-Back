8ef2cdeede68fd048b2c395f6bb4762fd67eb219
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-05-30 22:32:06 -0300

    style: ajustes php cs fixer

--- database/seeders/tenants/TypeUserSeeder.php
+++ database/seeders/tenants/TypeUserSeeder.php
@@ -14,19 +14,31 @@ class TypeUserSeeder extends Seeder
      */
     public function run()
     {
-        TypeUser::create([
-            'id' => 1,
-            'name' => 'Administrador',
-        ]);
+        TypeUser::updateOrCreate(
+            [
+                'id' => 1,
+            ],
+            [
+                'name' => 'Administrador',
+            ]
+        );
 
-        TypeUser::create([
-            'id' => 2,
-            'name' => 'Técnico',
-        ]);
+        TypeUser::updateOrCreate(
+            [
+                'id' => 2,
+            ],
+            [
+                'name' => 'Técnico',
+            ]
+        );
 
-        TypeUser::create([
-            'id' => 3,
-            'name' => 'Jogador',
-        ]);
+        TypeUser::updateOrCreate(
+            [
+                'id' => 3
+            ],
+            [
+                'name' => 'Jogador',
+            ]
+        );
     }
 }
