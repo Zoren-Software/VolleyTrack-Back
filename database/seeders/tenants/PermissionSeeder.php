4ada8f6d9f5cd5ad0736b11f27475f3bd692b346
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-06-06 22:19:15 -0300

    feat: adicionado permissões para tecnico

--- database/seeders/tenants/PermissionSeeder.php
+++ database/seeders/tenants/PermissionSeeder.php
@@ -15,10 +15,40 @@ class PermissionSeeder extends Seeder
      */
     public function run()
     {
+        /*
+         *Já estará como perfil de super administrador, e não precisará relacionar permissões neste perfil
+         */
         Role::updateOrCreate(['id' => 1], ['name' => 'Administrador']);
-        Role::updateOrCreate(['id' => 2], ['name' => 'Técnico']);
-        Role::updateOrCreate(['id' => 3], ['name' => 'Jogador']);
+
+        $technician =Role::updateOrCreate(['id' => 2], ['name' => 'Técnico']);
+        $player = Role::updateOrCreate(['id' => 3], ['name' => 'Jogador']);
         
-        Permission::updateOrCreate(['id' => 1], ['name' => 'edit articles']);
+        /*
+         * Permissões Usuário
+         */
+        $user[] = Permission::updateOrCreate(['id' => 1], ['name' => 'create user']);
+        $user[] = Permission::updateOrCreate(['id' => 2], ['name' => 'edit user']);
+        $user[] = Permission::updateOrCreate(['id' => 3], ['name' => 'list user']);
+        $user[] = Permission::updateOrCreate(['id' => 4], ['name' => 'list users']);
+
+        $this->sync($technician, $user);
+        
+        /*
+         * Permissões Time
+         */
+        $team[] = Permission::updateOrCreate(['id' => 5], ['name' => 'create team']);
+        $team[] = Permission::updateOrCreate(['id' => 6], ['name' => 'edit team']);
+        $team[] = Permission::updateOrCreate(['id' => 7], ['name' => 'list team']);
+        $team[] = Permission::updateOrCreate(['id' => 8], ['name' => 'list teams']);
+
+        $this->sync($technician, $user);
+        
+    }
+
+    public function sync($role, $permissions)
+    {
+        foreach($permissions as $permission) {
+            $role->givePermissionTo($permission);
+        }
     }
 }
