928abd51289414bccf392a552020db85ea64804e
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-06-17 13:25:55 -0300

    fix: ajustado criação usuário suporte

--- database/seeders/tenants/UserTableSeeder.php
+++ database/seeders/tenants/UserTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserTableSeeder extends Seeder
 {
@@ -17,11 +18,15 @@ class UserTableSeeder extends Seeder
     {
         //Criando e-mail admin do suporte
 
-        $user = \App\Models\User::updateOrCreate([
-            'name' => 'Suporte',
-            'email' => 'suporte@voleiclub.com.br', 
-            'password' => Hash::make('password'),
-            'email_verified_at' => now(),
-        ]);
+        $user = \App\Models\User::updateOrCreate(
+            ['id' => 1],
+            [
+                'name' => 'Suporte',
+                'email' => 'suporte@voleiclub.com', 
+                'password' => Hash::make('password'),
+                'email_verified_at' => now(),
+                'remember_token' => Str::random(10),
+            ]
+        );
     }
 }
