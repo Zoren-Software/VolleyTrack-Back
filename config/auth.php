ff971ef6331a0c46983148e6b32aa8a3bd38a731
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-06-17 12:31:02 -0300

    fix: ajustado configs do guard da aplicação

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'sanctum' => [
+            'driver' => 'sanctum',
+            'provider' => 'users',
+        ],
     ],
 
     /*
