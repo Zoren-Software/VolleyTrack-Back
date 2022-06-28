400123599182052a3c6a04f8d8f6fbdccfe5b418
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-06-28 09:53:49 -0300

    chore: removido código em comentário

--- database/seeders/tenants/DatabaseTenantSeeder.php
+++ database/seeders/tenants/DatabaseTenantSeeder.php
@@ -13,8 +13,6 @@ class DatabaseTenantSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
         $this->call([
             UserTableSeeder::class,
             PermissionSeeder::class,
