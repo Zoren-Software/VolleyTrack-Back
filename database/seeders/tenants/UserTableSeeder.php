436cbb5e1bec362964069dc40059bedbe14df9c2
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-06-28 10:26:07 -0300

    chore: ajuste duplicação de código

--- database/seeders/tenants/UserTableSeeder.php
+++ database/seeders/tenants/UserTableSeeder.php
@@ -15,61 +15,52 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //Criando e-mail admin e suporte
-
-        \App\Models\User::updateOrCreate(
-            ['id' => 1],
+        /*
+         * Criando e-mails admin e suporte
+         */
+        $usersDefault = [
             [
+                'id' => 1,
                 'name' => 'Suporte',
                 'email' => env('MAIL_FROM_ADDRESS'),
-                'password' => Hash::make('password'),
-                'email_verified_at' => now(),
-                'remember_token' => Str::random(10),
-            ]
-        );
-
-        \App\Models\User::updateOrCreate(
-            ['id' => 2],
+            ],
             [
+                'id' => 2,
                 'name' => 'Administrador',
                 'email' => env('MAIL_FROM_ADMIN'),
-                'password' => Hash::make('password'),
-                'email_verified_at' => now(),
-                'remember_token' => Str::random(10),
-            ]
-        );
+            ],
+        ];
 
-        //Criando e-mail de teste
+        /*
+         * Criando e-mails para testes
+         */
         if (env('APP_DEBUG')) {
-            \App\Models\User::updateOrCreate(
-                ['id' => 3],
-                [
-                    'name' => 'Usuário Teste Técnico',
-                    'email' => env('MAIL_FROM_TEST_TECHNICIAN'),
-                    'password' => Hash::make('password'),
-                    'email_verified_at' => now(),
-                    'remember_token' => Str::random(10),
-                ]
-            );
+            $usersDefault[] = [
+                'id' => 3,
+                'name' => 'Usuário Teste Técnico',
+                'email' => env('MAIL_FROM_TEST_TECHNICIAN'),
+            ];
 
-            \App\Models\User::updateOrCreate(
-                ['id' => 4],
-                [
-                    'name' => 'Usuário Teste Jogador',
-                    'email' => env('MAIL_FROM_TEST_PLAYER'),
-                    'password' => Hash::make('password'),
-                    'email_verified_at' => now(),
-                    'remember_token' => Str::random(10),
-                ]
-            );
+            $usersDefault[] = [
+                'id' => 4,
+                'name' => 'Usuário Teste Jogador',
+                'email' => env('MAIL_FROM_TEST_PLAYER'),
+            ];
 
-            \App\Models\User::updateOrCreate(
-                ['id' => 5],
+            $usersDefault[] = [
+                'id' => 5,
+                'name' => 'Usuário Sem Permissao',
+                'email' => env('MAIL_FROM_NO_PERMISSION'),
+            ];
+        }
+
+        foreach($usersDefault as $user) {
+            User::updateOrCreate(
+                ['id' => $user['id']],
                 [
-                    'name' => 'Usuário Sem Permissao',
-                    'email' => env('MAIL_FROM_NO_PERMISSION'),
+                    'name' => $user['name'],
+                    'email' => $user['email'],
                     'password' => Hash::make('password'),
-                    'email_verified_at' => now(),
                     'remember_token' => Str::random(10),
                 ]
             );
