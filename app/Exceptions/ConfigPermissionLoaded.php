3a8fe05839bb0faf5069691fd2c748c99eee6d2e
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-06-29 22:02:56 -0300

    chore: ajustado code smell removendo complexibilidade

---
+++ app/Exceptions/ConfigPermissionLoaded.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class ConfigPermissionLoaded extends Exception
+{
+        /**
+     * Report the exception.
+     *
+     * @return bool|null
+     */
+    public function report()
+    {
+        //
+    }
+
+    /**
+     * Render the exception into an HTTP response.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function render()
+    {
+        return 'Error: config/permission.php not loaded. Run [php artisan config:clear] and try again.';
+    }
+}
---
+++ app/Exceptions/VerifyColumnName.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class VerifyColumnName extends Exception
+{
+            /**
+     * Report the exception.
+     *
+     * @return bool|null
+     */
+    public function report()
+    {
+        //
+    }
+
+    /**
+     * Render the exception into an HTTP response.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function render()
+    {
+        return 'Error: team_foreign_key on config/permission.php not loaded. Run [php artisan config:clear] and try again.';
+    }
+}
