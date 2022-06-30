fe90cf9e26c1f4ae4083fdfec6e8205f6201c2e4
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-06-29 22:13:09 -0300

    chore: ajustado exception para diminuir a complexibilidade do código

---
+++ app/Exceptions/ConfigPermissionLoadedForDrop.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class ConfigPermissionLoadedForDrop extends Exception
+{
+            /**
+     * Report the exception.
+     *
+     * @return bool|null
+     */
+    public function report()
+    {
+        //
+    }
+
+    /**
+     * Render the exception into an HTTP response.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function render()
+    {
+        return 'Error: config/permission.php not found and defaults could not be merged. Please publish the package configuration before proceeding, or drop the tables manually.';
+    }
+}
