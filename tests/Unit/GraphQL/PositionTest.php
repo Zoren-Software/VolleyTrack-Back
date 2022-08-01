32370dc16af84128afc3a517786ca9c6fb7d5c10
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-07-31 21:18:27 -0300

    fix: ajuste no comando CI para não precisar mais manter dois arquivos de phpunit.xml

---
+++ tests/Unit/GraphQL/PositionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\GraphQL;
+
+use PHPUnit\Framework\TestCase;
+
+class PositionTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
