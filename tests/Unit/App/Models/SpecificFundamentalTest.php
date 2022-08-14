5ca527a96bb37a42f14c8b9eaebb20ec6a593cb4
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-08-14 16:54:08 -0300

    tests: adicionado testes unitários para todos os models

---
+++ tests/Unit/App/Models/SpecificFundamentalTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit\App\Models;
+
+use Tests\TestCase;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use App\Models\SpecificFundamental;
+
+class SpecificFundamentalTest extends TestCase
+{
+    /**
+     * A basic unit test relation users.
+     *
+     * @return void
+     */
+    public function test_user()
+    {
+        $specificFundamental = new SpecificFundamental();
+        $this->assertInstanceOf(BelongsTo::class, $specificFundamental->user());
+    }
+
+    /**
+     * A basic unit test relation users.
+     *
+     * @return void
+     */
+    public function test_fundamentals()
+    {
+        $specificFundamental = new SpecificFundamental();
+        $this->assertInstanceOf(BelongsToMany::class, $specificFundamental->fundamentals());
+    }
+}
