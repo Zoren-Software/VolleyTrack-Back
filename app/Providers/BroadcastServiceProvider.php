de7c0ba07676386feea543b4ee347ce9f3fb3c4d
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-10-03 21:46:21 -0300

    fix: ajustes bugfix

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -18,6 +18,6 @@ class BroadcastServiceProvider extends ServiceProvider
     {
         Broadcast::routes();
 
-        require base_path('routes/channels.php');
+        require_once base_path('routes/channels.php');
     }
 }
