b1f9a8e61b7e0ab7a96fee4286a8f3ea9264ba52
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-01-01 10:38:35 -0300

    feat: criado listagem de notificações para os usuários

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Notification extends Model
 {
     use HasFactory;
+
+    public static function list(array $args)
+    {
+        return Notification::userLogged()
+            ->filterRead($args['read'] ?? false)
+            ->orderBy('created_at', 'desc');
+    }
+
+    public function scopeUserLogged($query)
+    {
+        return $query->where('notifiable_id', auth()->user()->id);
+    }
+
+    public function scopeFilterRead($query, $read)
+    {
+        return $query->when(
+            $read === true,
+            fn ($query) => $query->whereNotNull('read_at')
+        )
+        ->when(
+            $read === false,
+            fn ($query) => $query->whereNull('read_at')
+        );
+    }
 }
