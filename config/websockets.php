85ff827c0488fe6854380b84af4d5d4fe2f032bb
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-12-31 22:04:28 -0300

    fix: removido alterações para ativar o websoket

--- config/websockets.php
+++ config/websockets.php
@@ -137,5 +137,5 @@ return [
      * The only requirement is that the class should implement
      * `ChannelManager` interface provided by this package.
      */
-    'channel_manager' => \App\WebSockets\Channels\ChannelManagers\ArrayChannelManager::class,
+    'channel_manager' => BeyondCode\LaravelWebSockets\Channels\ChannelManagers\ArrayChannelManager::class,
 ];
