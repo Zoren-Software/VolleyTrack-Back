822169d057cae8f325d3e9a80b8841327cdb8882
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-01-14 20:38:57 -0300

    feat: ativado modo noturno para o horizon

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -19,7 +19,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
         // Horizon::routeMailNotificationsTo('example@example.com');
         // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
 
-        // Horizon::night();
+        Horizon::night();
     }
 
     /**
