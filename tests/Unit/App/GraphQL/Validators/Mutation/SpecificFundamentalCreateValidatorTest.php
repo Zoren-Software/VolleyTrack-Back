8e6e9a18054df845308a909536cb6f05494f2f46
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-01-16 23:18:35 -0300

    test: ajustado testes unitários para fundamentos específicos, nas validações

--- tests/Unit/App/GraphQL/Validators/Mutation/SpecificFundamentalCreateValidatorTest.php
+++ tests/Unit/App/GraphQL/Validators/Mutation/SpecificFundamentalCreateValidatorTest.php
@@ -10,13 +10,28 @@ class SpecificFundamentalCreateValidatorTest extends TestCase
     /**
      * A basic unit test messages.
      *
+     * @test
      * @return void
      */
-    public function test_messages()
+    public function messages()
     {
         $validator = new SpecificFundamentalCreateValidator();
 
         $this->assertIsArray($validator->messages());
         $this->assertNotEmpty($validator->messages());
+    }
+
+    /**
+     * A basic unit test rules.
+     *
+     * @test
+     * @return void
+     */
+    public function rules()
+    {
+        $validator = new SpecificFundamentalCreateValidator();
+
+        $this->assertIsArray($validator->rules());
+        $this->assertNotEmpty($validator->rules());
     }
 }
