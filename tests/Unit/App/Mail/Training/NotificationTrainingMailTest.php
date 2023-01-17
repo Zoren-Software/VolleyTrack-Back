8f0a058af301658d035c33eb905642da666825bf
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-01-16 22:57:09 -0300

    test: adicionado testes unitários para funções de envio de e-mail

---
+++ tests/Unit/App/Mail/Training/NotificationTrainingMailTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Unit\App\Mail\Training;
+
+use Tests\TestCase;
+use App\Mail\Training\NotificationTrainingMail;
+use App\Models\Training;
+use App\Models\User;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Mail\Mailables\Content;
+
+class NotificationTrainingMailTest extends TestCase
+{
+    /**
+     * A method to test the envelope.
+     * @test
+     * @return void
+     */
+    public function envelope()
+    {
+        $trainingMock = $this->mock(Training::class, function ($mock) {
+            $mock->shouldReceive('getAttribute')->with('date_start')->andReturn(
+                // retornar a data de início do treino em formato Carbon
+                \Carbon\Carbon::parse('2020-01-01 00:00:00')
+            );
+            $mock->shouldReceive('getAttribute')->with('date_end')->andReturn(
+                // retornar a data de início do treino em formato Carbon
+                \Carbon\Carbon::parse('2020-01-01 00:00:00')
+            );
+
+        });
+        
+        $userMock = $this->createMock(User::class);
+        $mail = new NotificationTrainingMail($trainingMock, $userMock);
+        $envelope = $mail->envelope();
+
+        $this->assertInstanceOf(Envelope::class, $envelope);
+    }
+
+    /**
+     * A method to test the content.
+     * @test
+     * @return void
+     */
+    public function content()
+    {
+        $trainingMock = $this->mock(Training::class, function ($mock) {
+        });
+        
+        $userMock = $this->createMock(User::class);
+        $mail = new NotificationTrainingMail($trainingMock, $userMock);
+        $content = $mail->content();
+
+        $this->assertInstanceOf(Content::class, $content);
+    }
+}
