0138021b204780e1a91dcdf6adc1e86fdeb0c6de
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-01-17 13:37:37 -0300

    test: criado teste unitário para função que verifica alcance de datas para fazer as notificações para os usuários

--- tests/Unit/App/Models/TrainingTest.php
+++ tests/Unit/App/Models/TrainingTest.php
@@ -13,9 +13,10 @@ class TrainingTest extends TestCase
     /**
      * A basic unit test relation getActivitylogOptions.
      *
+     * @test
      * @return void
      */
-    public function test_get_activitylog_options()
+    public function getActivitylogOptions()
     {
         $training = new Training();
         $this->assertInstanceOf(LogOptions::class, $training->getActivitylogOptions());
@@ -24,9 +25,10 @@ class TrainingTest extends TestCase
     /**
      * A basic unit test relation user.
      *
+     * @test
      * @return void
      */
-    public function test_relation_user()
+    public function relationUser()
     {
         $training = new Training();
         $this->assertInstanceOf(BelongsTo::class, $training->user());
@@ -35,9 +37,10 @@ class TrainingTest extends TestCase
     /**
      * A basic unit test relation team.
      *
+     * @test
      * @return void
      */
-    public function test_relation_team()
+    public function relationTeam()
     {
         $training = new Training();
         $this->assertInstanceOf(BelongsTo::class, $training->team());
@@ -46,9 +49,10 @@ class TrainingTest extends TestCase
     /**
      * A basic unit test relation fundamentals.
      *
+     * @test
      * @return void
      */
-    public function test_relation_fundamentals()
+    public function relationFundamentals()
     {
         $training = new Training();
         $this->assertInstanceOf(BelongsToMany::class, $training->fundamentals());
@@ -57,11 +61,55 @@ class TrainingTest extends TestCase
     /**
      * A basic unit test relation specificFundamentals.
      *
+     * @test
      * @return void
      */
-    public function test_relation_specificFundamentals()
+    public function relationSpecificFundamentals()
     {
         $training = new Training();
         $this->assertInstanceOf(BelongsToMany::class, $training->specificFundamentals());
+    }
+
+    /**
+     * A basic unit test range date notification.
+     *
+     * @dataProvider rangeDateNotificationProvider
+     * @test
+     * @return void
+     */
+    public function rangeDateNotification(string $startDate, string $dateToday, string $dateLimit, bool $expected)
+    {
+        $training = new Training();
+        $this->assertEquals($expected, $training->rangeDateNotification($startDate, $dateToday, $dateLimit));
+    }
+
+    public function rangeDateNotificationProvider()
+    {
+        return [
+            'notification if training is on the day' => [
+                'startDate' => '15/01/2023',
+                'dateToday' => '15/01/2023',
+                'dateLimit' => '15/01/2023',
+                'expected' => true,
+            ],
+            'out-of-expected date range' => [
+                'startDate' => '17/01/2023',
+                'dateToday' => '15/01/2023',
+                'dateLimit' => '14/01/2023',
+                'expected' => false,
+            ],
+            'other out-of-expected date range' => [
+                'startDate' => '14/01/2023',
+                'dateToday' => '15/01/2023',
+                'dateLimit' => '18/01/2023',
+                'expected' => false,
+            ],
+            'in-expected date range' => [
+                'startDate' => '19/01/2023',
+                'dateToday' => '18/01/2023',
+                'dateLimit' => '20/01/2023',
+                'expected' => true,
+            ],
+        ];
     }
 }
