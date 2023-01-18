b9029ab76f2c68e0a6be920ef1049947a7bbaeda
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-01-17 23:01:28 -0300

    test: adicionado atributos a serem adicionados para testes unitários de classes de notificação

--- tests/Unit/App/Mail/Training/ConfirmationNotificationTrainingMailTest.php
+++ tests/Unit/App/Mail/Training/ConfirmationNotificationTrainingMailTest.php
@@ -20,12 +20,13 @@ class ConfirmationNotificationTrainingMailTest extends TestCase
     {
         $trainingMock = $this->mock(Training::class, function ($mock) {
             $mock->shouldReceive('getAttribute')->with('date_start')->andReturn(
-                // retornar a data de início do treino em formato Carbon
                 \Carbon\Carbon::parse('2020-01-01 00:00:00')
             );
             $mock->shouldReceive('getAttribute')->with('date_end')->andReturn(
-                // retornar a data de início do treino em formato Carbon
                 \Carbon\Carbon::parse('2020-01-01 00:00:00')
+            );
+            $mock->shouldReceive('getAttribute')->with('name')->andReturn(
+                'Test name'
             );
         });
 
@@ -43,7 +44,17 @@ class ConfirmationNotificationTrainingMailTest extends TestCase
      */
     public function content()
     {
-        $trainingMock = $this->createMock(Training::class);
+        $trainingMock = $this->mock(Training::class, function ($mock) {
+            $mock->shouldReceive('getAttribute')->with('date_start')->andReturn(
+                \Carbon\Carbon::parse('2020-01-01 00:00:00')
+            );
+            $mock->shouldReceive('getAttribute')->with('date_end')->andReturn(
+                \Carbon\Carbon::parse('2020-01-01 00:00:00')
+            );
+            $mock->shouldReceive('getAttribute')->with('name')->andReturn(
+                'Test name'
+            );
+        });
 
         $userMock = $this->createMock(User::class);
         $mail = new ConfirmationNotificationTrainingMail($trainingMock, $userMock);
