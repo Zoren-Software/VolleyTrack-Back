3cca6020e024ca97d311a9d37932353cdbd65bbd
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-01-24 18:00:24 -0300

    feat: adicionado permissão nas listagens de informações dos usuários

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -9,18 +9,51 @@ class UserPolicy
 {
     use HandlesAuthorization;
 
+    /**
+     * Create a new user instance.
+     * 
+     * @param User $user
+     * 
+     * @return bool
+     */
     public function create(User $user): bool
     {
         return $user->hasPermissionTo('edit-user');
     }
 
+    /**
+     * Edit a user instance.
+     * 
+     * @param User $user
+     * 
+     * @return bool
+     */
     public function edit(User $user): bool
     {
         return $user->hasPermissionTo('edit-user');
     }
 
+    /**
+     * Delete a user instance.
+     * 
+     * @param User $user
+     * 
+     * @return bool
+     */
     public function delete(User $user): bool
     {
         return $user->hasPermissionTo('edit-user');
+    }
+
+    /**
+     * View a user instance.
+     * 
+     * @param User $user
+     * 
+     * @return bool
+     */
+    public function view(User $user): bool
+    {
+        return $user->hasPermissionTo('edit-user') || $user->hasPermissionTo('view-user');
     }
 }
