6bc7fdb9f0ec367881e28282ae70ec0d0c0b2ed9
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-01-24 17:54:10 -0300

    feat: adicionado permissão para visualização de dados de configurações de treino

--- app/Policies/TrainingConfigPolicy.php
+++ app/Policies/TrainingConfigPolicy.php
@@ -9,8 +9,27 @@ class TrainingConfigPolicy
 {
     use HandlesAuthorization;
 
+    /**
+     * Edit a training config instance.
+     * 
+     * @param User $user
+     * 
+     * @return bool
+     */
     public function edit(User $user): bool
     {
         return $user->hasPermissionTo('edit-training-config');
+    }
+
+    /**
+     * View a training config instance.
+     * 
+     * @param User $user
+     * 
+     * @return bool
+     */
+    public function view(User $user): bool
+    {
+        return $user->hasPermissionTo('edit-training-config') || $user->hasPermissionTo('view-training-config');
     }
 }
