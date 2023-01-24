be76dd0c9ddd8544eac96975df3426c9e17119b5
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-01-24 17:38:00 -0300

    feat: adicionado permissão para rotas de visualização de dados

--- app/Policies/PositionPolicy.php
+++ app/Policies/PositionPolicy.php
@@ -3,6 +3,7 @@
 namespace App\Policies;
 
 use App\Models\User;
+use App\Models\Position;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class PositionPolicy
@@ -12,20 +13,41 @@ class PositionPolicy
     /**
      * Create a new policy instance.
      *
-     * @return void
+     * @return bool
      */
     public function create(User $user): bool
     {
         return $user->hasPermissionTo('edit-position');
     }
 
+    /**
+     * Edit a policy instance.
+     *
+     * @return bool
+     */
     public function edit(User $user): bool
     {
         return $user->hasPermissionTo('edit-position');
     }
 
+
+    /**
+     * Delete a policy instance.
+     *
+     * @return bool
+     */
     public function delete(User $user): bool
     {
         return $user->hasPermissionTo('edit-position');
+    }
+
+    /**
+     * View a policy instance.
+     *
+     * @return bool
+     */
+    public function view(User $user, Position $position): bool
+    {
+        return $user->hasPermissionTo('edit-position') || $user->hasPermissionTo('view-position');
     }
 }
