c3591d06457e4e4ec308338dab8a65348e159b87
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-01-24 17:52:27 -0300

    feat: adicionado permissão para as rotas de listagem de times

--- app/Policies/TeamPolicy.php
+++ app/Policies/TeamPolicy.php
@@ -9,18 +9,51 @@ class TeamPolicy
 {
     use HandlesAuthorization;
 
+    /**
+     * Create a new team instance.
+     * 
+     * @param User $user
+     * 
+     * @return bool
+     */
     public function create(User $user): bool
     {
         return $user->hasPermissionTo('edit-team');
     }
 
+    /**
+     * Edit a team instance.
+     * 
+     * @param User $user
+     * 
+     * @return bool
+     */
     public function edit(User $user): bool
     {
         return $user->hasPermissionTo('edit-team');
     }
 
+    /**
+     * Delete a team instance.
+     * 
+     * @param User $user
+     * 
+     * @return bool
+     */
     public function delete(User $user): bool
     {
         return $user->hasPermissionTo('edit-team');
+    }
+
+    /**
+     * View a team instance.
+     * 
+     * @param User $user
+     * 
+     * @return bool
+     */
+    public function view(User $user): bool
+    {
+        return $user->hasPermissionTo('edit-team') || $user->hasPermissionTo('view-team');
     }
 }
