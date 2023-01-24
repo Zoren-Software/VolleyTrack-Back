2caa6246bb49407e909ac5816c1f8d8b6b742efe
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-01-24 17:46:26 -0300

    feat: adicionado permissão em listagem de fundamentos específicos

--- app/Policies/SpecificFundamentalPolicy.php
+++ app/Policies/SpecificFundamentalPolicy.php
@@ -9,18 +9,51 @@ class SpecificFundamentalPolicy
 {
     use HandlesAuthorization;
 
+    /**
+     * Create a new specific fundamental instance.
+     * 
+     * @param User $user
+     * 
+     * @return bool
+     */
     public function create(User $user): bool
     {
         return $user->hasPermissionTo('edit-specific-fundamental');
     }
 
+    /**
+     * Edit a specific fundamental instance.
+     * 
+     * @param User $user
+     * 
+     * @return bool
+     */
     public function edit(User $user): bool
     {
         return $user->hasPermissionTo('edit-specific-fundamental');
     }
 
+    /**
+     * Delete a specific fundamental instance.
+     * 
+     * @param User $user
+     * 
+     * @return bool
+     */
     public function delete(User $user): bool
     {
         return $user->hasPermissionTo('edit-specific-fundamental');
+    }
+
+    /**
+     * View a specific fundamental instance.
+     * 
+     * @param User $user
+     * 
+     * @return bool
+     */
+    public function view(User $user): bool
+    {
+        return $user->hasPermissionTo('edit-specific-fundamental') || $user->hasPermissionTo('view-specific-fundamental');
     }
 }
