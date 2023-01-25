05240a3ed9eb4ef945ff751adeb92f61e16899af
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-01-25 18:56:56 -0300

    test: resolvendo problema da biblioteca faker para não gerar mais e-mails duplicados no banco de dados de teste

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -17,9 +18,15 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        //NOTE - Verificar manualmente se o email faker gerado é único, se não for, gerar outro
+        $email = $this->faker->unique()->safeEmail();
+        while (User::where('email', $email)->exists()) {
+            $email = $this->faker->unique()->safeEmail();
+        }
+
         return [
             'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'email' => $email,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
