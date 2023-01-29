9c6f2049f15182525807d128e0f3f44f78dfe240
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-01-28 21:07:46 -0300

    test: ajustado teste unitário para funções de confirmação

--- tests/Unit/App/Policies/ConfirmationTrainingPolicyTest.php
+++ tests/Unit/App/Policies/ConfirmationTrainingPolicyTest.php
@@ -30,4 +30,68 @@ class ConfirmationTrainingPolicyTest extends TestCase
 
         $this->assertEquals($expected, $confirmationTrainingPolicy->view($userMock));
     }
+
+    /**
+     * A basic unit test confirmTraining.
+     *
+     * @test
+     * 
+     * @dataProvider permissionProvider
+     * 
+     * @return void
+     */
+    public function permissionConfirmTraining(bool $expected): void
+    {
+        $args = [
+            'player_id' => 1,
+        ];
+
+        $userMock = $this->mock(User::class, function (MockInterface $mock) use ($args, $expected) {
+
+            $mock->shouldReceive('hasRoleAdmin')
+                ->andReturn($expected);
+
+            $mock->shouldReceive('hasRoleTechnician')
+                ->andReturn($expected);
+
+            $mock->shouldReceive('getAttribute')
+                ->with('id')
+                ->andReturn($args['player_id']);
+
+        });
+
+        $confirmationTrainingPolicy = new ConfirmationTrainingPolicy();
+
+        $this->assertEquals(true, $confirmationTrainingPolicy->confirmTraining($userMock, $args));
+    }
+
+    /**
+     * A basic unit test view.
+     *
+     * @test
+     * 
+     * @dataProvider permissionProvider
+     * 
+     * @return void
+     */
+    public function permissionConfirmPresence(bool $expected): void
+    {
+        $args = [
+            'player_id' => 1,
+        ];
+
+        $userMock = $this->mock(User::class, function (MockInterface $mock) use ($args, $expected) {
+
+            $mock->shouldReceive('hasRoleAdmin')
+                ->andReturn($expected);
+
+            $mock->shouldReceive('hasRoleTechnician')
+                ->andReturn($expected);
+
+        });
+
+        $confirmationTrainingPolicy = new ConfirmationTrainingPolicy();
+
+        $this->assertEquals($expected, $confirmationTrainingPolicy->confirmPresence($userMock, $args));
+    }
 }
