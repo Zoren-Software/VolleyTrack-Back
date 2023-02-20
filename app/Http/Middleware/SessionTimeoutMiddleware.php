635a513cd493b8d0feffb7fc6e68f2f82bd05fb1
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2023-02-20 17:04:46 -0300

    feat(laravel horizon): adicionando middleware para encerrar sessão após 120 min

---
+++ app/Http/Middleware/SessionTimeoutMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class SessionTimeoutMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $maxIdleTime = 120;
+
+        $lastActivity = session('last_activity');
+
+        if ($lastActivity && time() - $lastActivity > $maxIdleTime) {
+            auth()->guard('web')->logout();
+            session()->flush();
+
+            return redirect()->route('welcome-horizon');
+        }
+
+        session(['last_activity' => time()]);
+
+        return $next($request);
+    }
+}
