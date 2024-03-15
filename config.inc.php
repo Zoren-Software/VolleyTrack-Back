0778f1ad523df5c47af372969e1d402fc5391cb7
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2024-03-14 22:56:47 -0300

    feat: adicionado opção no container do phpmyadmin para conectar em outras instancias

---
+++ config.inc.php
@@ -0,0 +1,22 @@
+<?php
+
+/* Servidor: localhost */
+$i++;
+$cfg['Servers'][$i]['verbose'] = 'Development Local';
+$cfg['Servers'][$i]['host'] = 'mariadb';
+$cfg['Servers'][$i]['port'] = '3306';
+$cfg['Servers'][$i]['socket'] = '';
+$cfg['Servers'][$i]['connect_type'] = 'tcp';
+$cfg['Servers'][$i]['extension'] = 'mysqli';
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
+
+/* Servidor: Servidor Remoto */
+$i++;
+$cfg['Servers'][$i]['verbose'] = 'Production';
+$cfg['Servers'][$i]['host'] = 'volleytrack.cotjftyqknlg.us-east-1.rds.amazonaws.com';
+$cfg['Servers'][$i]['port'] = '3306'; // ou outra porta se necessário
+$cfg['Servers'][$i]['socket'] = '';
+$cfg['Servers'][$i]['connect_type'] = 'tcp';
+$cfg['Servers'][$i]['extension'] = 'mysqli';
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
