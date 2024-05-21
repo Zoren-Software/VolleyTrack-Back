91618634a4f2a378f62f5e2d56ed8270ed1a73cc
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2024-05-20 23:01:32 -0300

    feat(sistema): mostrando versão da aplicação (#106)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -148,6 +148,9 @@
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
+                    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                        Versão Aplicação {{ appVersion() }}
+                    </div>
                 </div>
             </div>
         </div>
