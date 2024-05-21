f3caeca73a2612f8660519f9f34e09a42d09de35
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2024-05-20 22:58:01 -0300

    feat: mostrando versão da aplicação

--- app/Helpers/Functions.php
+++ app/Helpers/Functions.php
@@ -4,3 +4,12 @@ function appHost()
 {
     return env('APP_HOST', 'volleytrack.com');
 }
+
+function appVersion()
+{
+    $composerJson = file_get_contents(base_path('composer.json'));
+
+    $composerVersao = json_decode($composerJson, true)['version'] ?? null;
+
+    return trim($composerVersao);
+}
