8c0dbfa6123032204f9129ac2369c13a5ab4e616
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2024-05-27 21:13:57 -0300

    feat(resetar senha): adicionando processo de resetar senha - VLT-159 (#111)
    * feat: criando funcionalidade para recuperar senha
    
    * chore: removido código em comentário
    
    * feat: fazendo o reset de senha
    
    * fix: ajuste para envio de e-mail e reset de senha
    
    * fix: ajustado teste funcional para forgotPassword
    
    * feat: feito tradução de modo correto dos termos mostrados no retorno da função
    
    * test: adicionado para a saída dos testes unitários os casos de testes feitos durante a execução

---
+++ app/Mail/User/ForgotPasswordMail.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Mail\User;
+
+use App\Models\User;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+
+class ForgotPasswordMail extends Mail
+{
+    /**
+     * The title of the mail.
+     *
+     * @var string
+     */
+    public $title;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, string $tenant)
+    {
+        parent::__construct($user, $tenant);
+
+        $this->title = 'Recuperação de senha';
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: env('APP_NAME') .
+            ' - ' . 'Recuperação de senha'
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            markdown: 'emails.user.forgot-password-email',
+        );
+    }
+}
---
+++ lang/pt-br/ForgotPasswordMail.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'title_mail' => 'Recuperação de senha',
+    'hello' => 'Olá',
+    'access_details' => 'Aqui estão os detalhes de acesso ao sistema',
+    'email' => 'E-mail',
+    'thanks' => 'Atenciosamente',
+    'why' => 'Você está recebendo este e-mail porque alguma pessoa solicitou a recuperação de senha. ' .
+        'Caso não tenha sido você, por favor, ignore este e-mail.',
+    'message_support' => 'Por favor, não hesite em nos contatar em caso de dúvidas ' .
+        'ou necessidade de assistência adicional.',
+    'button_forgot_password' => 'Recuperar senha',
+];
