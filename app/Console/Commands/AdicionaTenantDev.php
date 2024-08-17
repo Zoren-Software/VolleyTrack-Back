b6a9c295f8573bfef1951bfba42f522c79fe97ef
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2024-08-17 00:25:19 -0300

    feat(tenant): adicionando atalho dev resetar senhas e criar tenant (#116)
    * feat: adicionado novos comandos para importação de bancos de dados, criação de tenants e reset de senhas para ambiente de desenvolvimento
    
    * chore: ajustes php cs fixer

---
+++ app/Console/Commands/AdicionaTenantDev.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+class AdicionaTenantDev extends CommandDev
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dev:add-tenant {--tenants=*}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Add a new tenant to the database Central';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $handle = parent::handle();
+
+        if ($handle === false) {
+            return false;
+        }
+
+        if (!$this->option('tenants')) {
+            $tenants[] = $this->ask('What is the tenant id?');
+        } else {
+            $tenants = $this->option('tenants');
+        }
+
+        foreach ($tenants as $tenant) {
+            if (\App\Models\Tenant::find($tenant)) {
+                $this->error("Tenant {$tenant} already exists!");
+
+                continue;
+            }
+
+            $tenant = \App\Models\Tenant::create([
+                'id' => $tenant,
+            ]);
+
+            $this->info("Tenant {$tenant->id} added successfully!");
+
+            $tenant->domains()->create(['domain' => $tenant->id . '.' . env('APP_HOST')]);
+
+            $this->info('Default domain added successfully!');
+            $this->info("{$tenant->id}" . '.' . env('APP_HOST'));
+        }
+    }
+}
