ccb6dc08a9404b114f231bdeead1b353e7a0628b
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2024-08-16 23:57:28 -0300

    feat: adicionado novos comandos para importação de bancos de dados, criação de tenants e reset de senhas para ambiente de desenvolvimento

---
+++ app/Console/Commands/CommandBase.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CommandBase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    protected $tenant;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->tenant = null;
+    }
+
+    public function formatProgress($tenant)
+    {
+        return "cron - tenant($tenant): PROCESSO $this->nomeProcesso %current%/%max% [%bar%] %percent:3s%% estimated: %estimated:-6s% - current: %elapsed:-3s% \n";
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+    }
+
+    /**
+     * @param mixed $nomeComando
+     * @param mixed $mensagem
+     *
+     * @return void
+     */
+    protected function infoComando($nomeComando, $mensagem): void
+    {
+        if (env('APP_ENV') == 'test') {
+            return;
+        }
+        $this->info('cron - ' . $mensagem . ': ' . $nomeComando);
+    }
+
+    /**
+     * @param mixed $nomeComando
+     * @param mixed $tenant
+     * @param mixed $mensagem
+     *
+     * @return void
+     */
+    protected function processoComando($nomeComando, $tenant, $mensagem): void
+    {
+        if (env('APP_ENV') == 'test') {
+            return;
+        }
+        $this->info('cron - tenant(' . $tenant . '): ' . $mensagem . ' PROCESSO ' . $nomeComando);
+    }
+
+    /**
+     * @param mixed $nomeComando
+     * @param mixed $tenant
+     * @param mixed $mensagem
+     *
+     * @return void
+     */
+    protected function processoComandoCancelado($nomeComando, $tenant, $mensagem): void
+    {
+        if (env('APP_ENV') == 'test') {
+            return;
+        }
+        $this->warn('cron - tenant(' . $tenant . '): ' . $mensagem . ' PROCESSO ' . $nomeComando);
+    }
+
+    /**
+     * @param mixed $nomeComando
+     * @param mixed $tenant
+     * @param mixed $seeder
+     *
+     * @return void
+     */
+    protected function execSeederComando($nomeComando, $tenant, $seeder): void
+    {
+        if (env('APP_ENV') == 'test') {
+            return;
+        }
+        $this->info('cron - tenant(' . $tenant . '): seeder: ' . $seeder . ' PROCESSO ' . $nomeComando);
+    }
+}
---
+++ app/Console/Commands/CommandDev.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+class CommandDev extends CommandBase
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dev:new_command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Description command base';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        if (env('APP_ENV') == 'production') {
+            $this->error('It is not possible to run this command in the production environment');
+            return false;
+        }
+
+        if (!(env('APP_DEBUG')) || env('APP_ENV') == 'staging') {
+            $this->error('It is not possible to run this command in this environment');
+            return false;
+        }
+    }
+}
