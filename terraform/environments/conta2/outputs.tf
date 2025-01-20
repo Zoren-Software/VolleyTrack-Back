1c31e35b6df2e80a45d6b1e74fda8b6da438623e
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2025-01-19 23:26:35 -0300

    feat: aprimorar a saída do endpoint do RDS para incluir suporte a regex

--- terraform/environments/conta2/outputs.tf
+++ terraform/environments/conta2/outputs.tf
@@ -1,6 +1,10 @@
 output "rds_endpoint" {
-    description = "Endpoint do RDS MySQL"
-    value       = module.rds_conta2.db_endpoint
+  description = "Endpoint do RDS MySQL"
+  value = (
+    can(regex("^(.+?):[0-9]+$", module.rds_conta2.db_endpoint))
+    ? regex("^(.+?):[0-9]+$", module.rds_conta2.db_endpoint)[0]
+    : module.rds_conta2.db_endpoint
+  )
 }
 
 output "redis_endpoint" {
