0b1b4da00070de86e4ec70e7815c75920a9964c3
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2025-02-02 23:55:51 -0300

    feat: refatorar migrações da tabela fundamentals_trainings para verificar a existência da tabela, adicionar chaves estrangeiras e índices

---
+++ database/migrations/tenant/base/2024_02_02_000028_add_foreign_keys_to_fundamentals_trainings_table.php
@@ -0,0 +1,70 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('fundamentals_trainings')) {
+            Schema::table('fundamentals_trainings', function (Blueprint $table) {
+                if (!hasForeignKeyExist('fundamentals_trainings', 'fundamentals_trainings_fundamental_id_foreign')) {
+                    $table->foreign('fundamental_id', 'fundamentals_trainings_fundamental_id_foreign')
+                        ->references('id')
+                        ->on('fundamentals')
+                        ->onDelete('cascade');
+                }
+
+                if (!hasForeignKeyExist('fundamentals_trainings', 'fundamentals_trainings_training_id_foreign')) {
+                    $table->foreign('training_id', 'fundamentals_trainings_training_id_foreign')
+                        ->references('id')
+                        ->on('trainings')
+                        ->onDelete('cascade');
+                }
+
+                if (!hasIndexExist('fundamentals_trainings', 'fundamentals_trainings_fundamental_id_index')) {
+                    $table->index('fundamental_id', 'fundamentals_trainings_fundamental_id_index');
+                }
+
+                if (!hasIndexExist('fundamentals_trainings', 'fundamentals_trainings_training_id_index')) {
+                    $table->index('training_id', 'fundamentals_trainings_training_id_index');
+                }
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasTable('fundamentals_trainings')) {
+            Schema::table('fundamentals_trainings', function (Blueprint $table) {
+                if (hasForeignKeyExist('fundamentals_trainings', 'fundamentals_trainings_fundamental_id_foreign')) {
+                    $table->dropForeign('fundamentals_trainings_fundamental_id_foreign');
+                }
+
+                if (hasForeignKeyExist('fundamentals_trainings', 'fundamentals_trainings_training_id_foreign')) {
+                    $table->dropForeign('fundamentals_trainings_training_id_foreign');
+                }
+
+                if (hasIndexExist('fundamentals_trainings', 'fundamentals_trainings_fundamental_id_index')) {
+                    $table->dropIndex('fundamentals_trainings_fundamental_id_index');
+                }
+
+                if (hasIndexExist('fundamentals_trainings', 'fundamentals_trainings_training_id_index')) {
+                    $table->dropIndex('fundamentals_trainings_training_id_index');
+                }
+            });
+        }
+    }
+};
