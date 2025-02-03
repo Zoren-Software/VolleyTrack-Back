1e2fd1ebf9743c8de948801e6bc1ba71712e1e81
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2025-02-02 23:16:24 -0300

    feat: adicionar funções para verificar a existência de chaves estrangeiras, índices e eventos no banco de dados

--- app/Helpers/Functions.php
+++ app/Helpers/Functions.php
@@ -13,3 +13,44 @@ function appVersion()
 
     return trim($composerVersao);
 }
+
+function hasForeignKeyExist($table, $nameForeignKey)
+{
+    $conn = Schema::getConnection()->getDoctrineSchemaManager();
+
+    $foreignKeys = array_map(function ($key) {
+        return $key->getName();
+    }, $conn->listTableForeignKeys($table));
+
+    return in_array($nameForeignKey, $foreignKeys);
+}
+
+function hasIndexExist($table, $nameIndex)
+{
+    $conn = Schema::getConnection()->getDoctrineSchemaManager();
+
+    $index = array_map(function ($key) {
+        return $key->getName();
+    }, $conn->listTableIndexes($table));
+
+    return in_array($nameIndex, $index);
+}
+
+function hasEventExist($eventName)
+{
+    // Obter o nome do banco de dados do tenant ativo
+    $dbName = DB::connection()->getDatabaseName();
+
+    if (!$dbName) {
+        throw new Exception('Nenhum banco de dados encontrado para o tenant ativo.');
+    }
+
+    $result = DB::connection()->select('
+        SELECT EVENT_NAME
+        FROM INFORMATION_SCHEMA.EVENTS
+        WHERE EVENT_SCHEMA = ? 
+            AND EVENT_NAME = ?
+    ', [$dbName, $eventName]);
+
+    return !empty($result);
+}
