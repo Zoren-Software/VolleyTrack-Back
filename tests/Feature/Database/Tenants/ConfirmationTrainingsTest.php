5993f9a2b8e20bb2ea8224e26b4fa5aeb82e2881
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2025-02-17 23:02:12 -0300

    feat: adicionar teste para a tabela confirmation_trainings com definição de chaves primárias e estrangeiras

---
+++ tests/Feature/Database/Tenants/ConfirmationTrainingsTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature\Database\Tenants;
+
+use Tests\TestCase;
+
+class ConfirmationTrainingsTest extends TenantBase
+{
+    protected $table = 'confirmation_trainings';
+
+    public static $fields = [
+        'id',
+        'user_id',
+        'player_id',
+        'training_id',
+        'team_id',
+        'status',
+        'presence',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    public static $primaryKey = ['id']; // Define a chave primária
+
+    public static $autoIncrements = ['id']; // Define quais campos são auto_increment
+
+    public static $foreignKeys = [
+        'confirmation_trainings_user_id_foreign',
+        'confirmation_trainings_team_id_foreign',
+        'confirmation_trainings_training_id_foreign'
+    ]; // Define as chaves estrangeiras
+
+    public static $uniqueKeys = [];
+}
