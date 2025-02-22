e8889488a55f88c3212cda7c692b0f6d7a7d387f
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2025-02-22 09:15:43 -0300

    refactor: adicionar exceção personalizada para configuração de permissões não carregada

--- database/migrations/tenant/base/2025_02_12_000001_create_permissions_table.php
+++ database/migrations/tenant/base/2025_02_12_000001_create_permissions_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Exceptions\PermissionConfigNotLoadedException;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,7 +12,8 @@ return new class extends Migration
         $tableNames = config('permission.table_names');
 
         if (empty($tableNames)) {
-            throw new \Exception('Error: config/permission.php not loaded. Run [php artisan config:clear] and try again.');
+            // Lançando a exceção personalizada
+            throw new PermissionConfigNotLoadedException();
         }
 
         if (!Schema::hasTable($tableNames['permissions'])) {
