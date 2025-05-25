4166757cf055a57574edc5699d921300d02d3c85
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2025-05-25 16:49:12 -0300

    refactor: melhora a formatação de mensagens de progresso e atualiza verificação de ambiente para usar a configuração do aplicativo

--- app/Console/Commands/CommandBase.php
+++ app/Console/Commands/CommandBase.php
@@ -20,78 +20,64 @@ class CommandBase extends Command
      */
     protected $description = 'Command description';
 
+    /**
+     * Nome do processo utilizado em mensagens de progresso
+     *
+     * @var string
+     */
+    protected string $nomeProcesso = 'default';
+
     protected $tenant;
 
     public function __construct()
     {
         parent::__construct();
-
         $this->tenant = null;
     }
 
-    public function formatProgress($tenant)
+    public function formatProgress($tenant): string
     {
-        return "cron - tenant($tenant): PROCESSO $this->nomeProcesso %current%/%max% [%bar%] %percent:3s%% estimated: %estimated:-6s% - current: %elapsed:-3s% \n";
+        return "cron - tenant($tenant): PROCESSO {$this->nomeProcesso} %current%/%max% [%bar%] %percent:3s%% estimated: %estimated:-6s% - current: %elapsed:-3s%\n";
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle()
     {
         //
     }
 
-    /**
-     * @param  mixed  $nomeComando
-     * @param  mixed  $mensagem
-     */
     protected function infoComando($nomeComando, $mensagem): void
     {
-        if (env('APP_ENV') == 'test') {
+        if (config('app.env') === 'test') {
             return;
         }
+
         $this->info('cron - ' . $mensagem . ': ' . $nomeComando);
     }
 
-    /**
-     * @param  mixed  $nomeComando
-     * @param  mixed  $tenant
-     * @param  mixed  $mensagem
-     */
     protected function processoComando($nomeComando, $tenant, $mensagem): void
     {
-        if (env('APP_ENV') == 'test') {
+        if (config('app.env') === 'test') {
             return;
         }
+
         $this->info('cron - tenant(' . $tenant . '): ' . $mensagem . ' PROCESSO ' . $nomeComando);
     }
 
-    /**
-     * @param  mixed  $nomeComando
-     * @param  mixed  $tenant
-     * @param  mixed  $mensagem
-     */
     protected function processoComandoCancelado($nomeComando, $tenant, $mensagem): void
     {
-        if (env('APP_ENV') == 'test') {
+        if (config('app.env') === 'test') {
             return;
         }
+
         $this->warn('cron - tenant(' . $tenant . '): ' . $mensagem . ' PROCESSO ' . $nomeComando);
     }
 
-    /**
-     * @param  mixed  $nomeComando
-     * @param  mixed  $tenant
-     * @param  mixed  $seeder
-     */
     protected function execSeederComando($nomeComando, $tenant, $seeder): void
     {
-        if (env('APP_ENV') == 'test') {
+        if (config('app.env') === 'test') {
             return;
         }
+
         $this->info('cron - tenant(' . $tenant . '): seeder: ' . $seeder . ' PROCESSO ' . $nomeComando);
     }
 }
